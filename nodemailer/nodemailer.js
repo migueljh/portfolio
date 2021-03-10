64551066c001d5f1cf4f9080659e1fc2dacb635d
Miguel Hernandez <miguelhernandezjp@gmail.com>
2021-03-10 19:48:15 -0300

    wefw

--- nodemailer/nodemailer.js
+++ nodemailer/nodemailer.js
@@ -6,7 +6,7 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use(cors());
 
-server.post("/send-email", (req, res) => {
+server.post("/send-email", (req, res) => { 
   const { name, email, message } = req.body;
 
   const transporter = nodemailer.createTransport({
